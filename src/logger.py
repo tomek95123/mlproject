e97add48c9aa2c4bf4ff199a448b199afcc68d73
Tomasz Faber <tomekfaber@onet.pl>
2023-09-12 15:53:46 +0200

    Data Ingestion

--- src/logger.py
+++ src/logger.py
@@ -7,7 +7,6 @@ logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
 os.makedirs(logs_path, exist_ok=True)
 
 LOG_FILE_PATH=os.path.join(logs_path, LOG_FILE)
-print(LOG_FILE_PATH)
 logging.basicConfig(
     filename=LOG_FILE_PATH,
     format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
